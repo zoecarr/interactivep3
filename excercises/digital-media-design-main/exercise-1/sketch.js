2c450dfcdf83de33de9ae7430d686f754c74d2e6
zoecarr <125209206+zoecarr@users.noreply.github.com>
2023-03-29 10:37:53 -0700

    organizing excercises folder

--- excercises/digital-media-design-main/exercise-1/sketch.js
+++ excercises/digital-media-design-main/exercise-1/sketch.js
@@ -9,19 +9,20 @@ function setup() {
   }
   
   function draw() {
-    //removeElements();
+    //gets rid of the trail while the image moves
+     //removeElements();
 
     //draw a white circle in the middle of the page
-    // fill(255);
-    // ellipse(width/2, height/2, 100,100);
+    //fill(255);
+    //ellipse(width/2, height/2, 100,100);
 
     //this is how to use p5.js and javascript to generate images into html elements
 
     //create an image element in html
-    let cat = createImg("images/cat.png");
+    let cat = createImg("images/cyanotype.jpeg");
 
     //select size of image
-    cat.size(200,200);
+    cat.size(600,600);
 
     //set the position
     cat.position(500, 500);
@@ -32,17 +33,17 @@ function setup() {
     //now you can see that in your html, an <img> tag is created that has an id of "cat", has a size of 200x200, and is positioned at 500x500 on the page. 
 
     //lets make this follow your cursor
-    //cat.position(mouseX, mouseY);
+    cat.position(mouseX, mouseY);
 
     //lets make this cat move
-    //cat.position((200 + a), 500);
+    cat.position((200 + a), 500);
 
 
     //lets make this cat move without leaving a trail - removeElements(); up at the top
-    // cat.position((200 + a), 500);
+    cat.position((200 + a), 500);
 
-    //a++;
-    //console.log(a);
+    //short notation for adding one each from
+    a++;
 
     //make the bear appear when pressed
     cat.mousePressed(bear);
@@ -55,7 +56,7 @@ function setup() {
 }
 
 function bear(){
-  let bear = createImg("images/bearhug.png");
+  let bear = createImg("images/cyanotype.jpeg");
   bear.size(400,400);
   bear.position(800, 400);
 }
