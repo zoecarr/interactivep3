2f709e03e18cc35c94ffa3221903c86ecd642ba4
zoecarr <125209206+zoecarr@users.noreply.github.com>
2023-04-28 17:48:32 -0700

    trying to make shapes appear when key is clicked

---
+++ sketch2.js
@@ -0,0 +1,38 @@
+
+let mySound1, mySound2, mySound3, mySound4, mySound5, mySound6;
+
+let shape1;
+
+let image;
+
+function preload(){
+
+  soundFormats('mp3', 'ogg');
+    mySound1 = loadSound('../excercises/exercise-4/audio/Plantasia.mp3');
+    mySound2 = loadSound('../excercises/exercise-4/audio/soundbowl_F.mp3');
+    mySound3 = loadSound('../excercises/exercise-4/audio/soundbowl1.mp3');
+    mySound4 = loadSound('../excercises/exercise-4/audio/soundbowl_inG.mp3');
+    mySound5 = loadSound('../excercises/exercise-4/audio/soundbowl_Fx3.mp3');
+    mySound6 = loadSound('../excercises/exercise-4/audio/soundbowl_v2.mp3');
+
+    shape1 = createImg('/images/shapes/A.png');
+}
+
+function setup() {
+  let cnv = createCanvas(windowWidth, windowHeight);
+  cnv.parent('mySketch2');
+
+  shape1 = createImg("/images/shapes/A.png");
+    shape1.class('shapes');
+}
+
+function draw(){
+  background(355);
+}
+
+function keyPressed(){
+  if (key === 'a'){
+    mySound1.play();
+    shape1(50,50,50,50);
+  }
+}
